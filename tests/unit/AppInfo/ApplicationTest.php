1dff6211678c0f2b6c1e75a367848b40f52ea6a7
Misha M.-Kupriyanov <kupriyanov@strato.de>
2024-09-12 16:45:31 +0200

    feat(theming): add initial default theme override
    Signed-off-by: Misha M.-Kupriyanov <kupriyanov@strato.de>

---
+++ tests/unit/AppInfo/ApplicationTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPDX-FileLicenseText: 2024 STRATO AG
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+namespace OCA\NcTheming\Tests\AppInfo;
+
+use OC\AppFramework\Bootstrap\Coordinator;
+use OCA\NcTheming\AppInfo\Application;
+use OCA\NcTheming\Service\NcThemesService;
+use OCA\Theming\Service\ThemesService;
+use PHPUnit\Framework\TestCase;
+
+class ApplicationTest extends TestCase {
+	private Application $application;
+
+	protected function setUp(): void {
+		parent::setUp();
+
+		$this->application = new Application();
+		$coordinator = \OC::$server->get(Coordinator::class);
+		$this->application->register($coordinator->getRegistrationContext()->for('nc_theming'));
+	}
+
+	public function testThemingOverrideRegistration(): void {
+		$themesService = \OC::$server->getRegisteredAppContainer('theming')->get(ThemesService::class);
+		$this->assertInstanceOf(NcThemesService::class, $themesService, 'FATAL: NcThemesService is not registered!');
+	}
+}
