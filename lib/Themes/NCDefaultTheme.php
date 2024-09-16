61172e0eb0c3f3ce8561327921da131f0293a7d9
Kai Henseler <kai.henseler@strato.de>
2024-09-16 12:27:42 +0200

    style(NcDefaultTheme): color mapping
    Signed-off-by: Kai Henseler <kai.henseler@strato.de>

--- lib/Themes/NCDefaultTheme.php
+++ lib/Themes/NCDefaultTheme.php
@@ -115,9 +115,120 @@ class NCDefaultTheme extends DefaultTheme implements ITheme {
 			'--ion-color-amber-y3' => $ionColorAmberY3,
 		];
 
+		// COLOR MAPPING
+		$colorMainText = $ionColorTypoMild;
+		$colorMainTextRgb = join(',', $this->util->hexToRGB($colorMainText));
+		$colorTextMaxcontrast = $ionColorTypoMild;
+		
+
+		$colorMainBackground = '#fff';
+		$colorMainBackgroundRGB = join(',', $this->util->hexToRGB($colorMainBackground));
+		$colorBoxShadow = $this->util->darken($colorMainBackground, 70);
+		$colorBoxShadowRGB = join(',', $this->util->hexToRGB($colorBoxShadow));
+
+		$colorError = $ionColorRoseR3;
+		$colorWarning = $ionColorAmberY3;
+		$colorSuccess = $ionColorGreenG3;
+		$colorInfo = $ionColorSkyS3;
+	
+		$variables = [
+			'--color-main-background' => $colorMainBackground,
+			'--color-main-background-rgb' => $colorMainBackgroundRGB,
+			'--color-main-background-translucent' => 'rgba(var(--color-main-background-rgb), .97)',
+			'--color-main-background-blur' => 'rgba(var(--color-main-background-rgb), .8)',
+
+			// to use like this: background-image: linear-gradient(0, var('--gradient-main-background));
+			'--gradient-main-background' => 'var(--color-main-background) 0%, var(--color-main-background-translucent) 85%, transparent 100%',
+
+			// used for different active/hover/focus/disabled states
+			'--color-background-hover' => 'rgba(248, 248, 248, 1)',
+			'--color-background-dark' => $this->util->darken($colorMainBackground, 7),
+			'--color-background-darker' => $this->util->darken($colorMainBackground, 14),
+
+			'--color-placeholder-light' => $this->util->darken($colorMainBackground, 10),
+			'--color-placeholder-dark' => $this->util->darken($colorMainBackground, 20),
+
+			// max contrast for WCAG compliance
+			'--color-main-text' => $colorMainText,
+			'--color-text-maxcontrast' => $colorTextMaxcontrast,
+			'--color-text-maxcontrast-default' => $colorTextMaxcontrast,
+			'--color-text-maxcontrast-background-blur' => $this->util->darken($colorTextMaxcontrast, 7),
+			'--color-text-light' => 'var(--color-main-text)', // deprecated
+			'--color-text-lighter' => 'var(--color-text-maxcontrast)', // deprecated
+
+			'--color-scrollbar' => 'rgba(' . $colorMainTextRgb . ', .15)',
+
+			// error/warning/success/info feedback colours
+			'--color-error' => $colorError,
+			'--color-error-rgb' => join(',', $this->util->hexToRGB($colorError)),
+			'--color-error-hover' => $this->util->mix($colorError, $colorMainBackground, 75),
+			'--color-error-text' => $this->util->darken($colorError, 5),
+			'--color-warning' => $colorWarning,
+			'--color-warning-rgb' => join(',', $this->util->hexToRGB($colorWarning)),
+			'--color-warning-hover' => $this->util->darken($colorWarning, 5),
+			'--color-warning-text' => $this->util->darken($colorWarning, 7),
+			'--color-success' => $colorSuccess,
+			'--color-success-rgb' => join(',', $this->util->hexToRGB($colorSuccess)),
+			'--color-success-hover' => $this->util->mix($colorSuccess, $colorMainBackground, 80),
+			'--color-success-text' => $this->util->darken($colorSuccess, 4),
+			'--color-info' => $colorInfo,
+			'--color-info-rgb' => join(',', $this->util->hexToRGB($colorInfo)),
+			'--color-info-hover' => $this->util->mix($colorInfo, $colorMainBackground, 80),
+			'--color-info-text' => $this->util->darken($colorInfo, 4),
+			'--color-favorite' => $ionColorAmberY3,
+			// used for the icon loading animation
+			'--color-loading-light' => '#cccccc',
+			'--color-loading-dark' => '#444444',
+
+			'--color-box-shadow-rgb' => $colorBoxShadowRGB,
+			'--color-box-shadow' => "rgba(var(--color-box-shadow-rgb), 0.5)",
+
+			'--color-border' => $this->util->darken($colorMainBackground, 7),
+			'--color-border-dark' => $this->util->darken($colorMainBackground, 14),
+			'--color-border-maxcontrast' => $this->util->darken($colorMainBackground, 51),
+
+			'--default-font-size' => '15px',
+
+			// TODO: support "(prefers-reduced-motion)"
+			'--animation-quick' => '100ms',
+			'--animation-slow' => '300ms',
+
+			// Default variables --------------------------------------------
+			// Border width for input elements such as text fields and selects
+			'--border-width-input' => '1px',
+			'--border-width-input-focused' => '2px',
+			'--border-radius' => '3px',
+			'--border-radius-large' => '10px',
+			'--border-radius-rounded' => '28px',
+			// pill-style button, value is large so big buttons also have correct roundness
+			'--border-radius-pill' => '100px',
+
+			'--default-clickable-area' => '44px',
+			'--default-line-height' => '24px',
+			'--default-grid-baseline' => '4px',
+
+			// various structure data
+			'--header-height' => '50px',
+			'--navigation-width' => '300px',
+			'--sidebar-min-width' => '300px',
+			'--sidebar-max-width' => '500px',
+			'--list-min-width' => '200px',
+			'--list-max-width' => '300px',
+			'--header-menu-item-height' => '44px',
+			'--header-menu-profile-item-height' => '66px',
+
+			// mobile. Keep in sync with core/js/js.js
+			'--breakpoint-mobile' => '1024px',
+			'--background-invert-if-dark' => 'no',
+			'--background-invert-if-bright' => 'invert(100%)',
+			'--background-image-invert-if-bright' => 'no',
+			'--background-image-color-text' => '#ffffff',
+		];
+
 		return array_merge(
 			$defaultVariables,
 			$ionosVariables,
+			$variables,
 			[
 				'--font-face' => '"Open sans", ' . $originalFontFace
 			]
